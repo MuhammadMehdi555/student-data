465dc6a8a388f519c9ac19c06c136a84fad1eaa1
Muhammad Mehdi <muhammadmehdi0555@gmail.com>
2025-04-25 22:54:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var d1=document.getElementById('d1')
+var d2=document.getElementById('d2')
+var d3=document.getElementById('d3')
+var d4=document.getElementById('d4')
+function no1(){
+    d1.style.display="none"
+    }
+function no2(){
+    d2.style.display="none"
+    }
+function no3(){
+    d3.style.display="none"
+    }
+function no4(){
+    d4.style.display="none"
+    }
